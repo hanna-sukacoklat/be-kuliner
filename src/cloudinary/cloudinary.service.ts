9bd008b895bdadb91a2a3212f3755751d36582d8
Gloria Hanna Prasetyo <gloria_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2026-06-02 07:12:00 +0700

    add cloudinary

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,31 @@
+/// <reference types="multer" />
+import { Injectable } from '@nestjs/common';
+import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+import cloudinary from 'src/halper/cloudinary.config';
+import type { Express } from 'express';
+
+@Injectable()
+export class CloudinaryService {
+    async uploadFile(file: Express.Multer.File, folder: string):
+    Promise<UploadApiErrorResponse | UploadApiResponse> {
+
+        return await new Promise((resolve, reject) => {
+            const upload_stream = cloudinary.uploader.upload_stream(
+                { folder: folder },
+                (error, result) => {
+                    if (error) return reject(error);
+                    resolve(result!);
+                }
+            );
+            upload_stream.end(file.buffer);
+        });
+    }
+
+    async deletFile(url: string) {
+        const slices = url.split('/');
+        const filenameWithExtension = slices[slices.length - 1];
+        const [filename] = filenameWithExtension.split('.');
+        return await cloudinary.uploader.destroy(filename);
+    }
+
+}
